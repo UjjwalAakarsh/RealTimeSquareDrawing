ae327f75241bcc2b7fcd6bf9fca6d4fcf5bea4b2
UjjwalAakarsh <72330364+UjjwalAakarsh@users.noreply.github.com>
2021-04-14 19:39:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+video=createCapture(VIDEO)
+video.size(500,500)
+canvas=createCanvas(500,500)
+canvas.position(750,200)
+video.position(200,200)
+}
+function preload(){
+
+}
+function draw(){
+
+}
